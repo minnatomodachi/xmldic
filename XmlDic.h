4c78d5fc790109be33273ba37e599d2ec433658d
shigenari <arai.shigenari@minnatomodachi.net>
2012-11-15 17:52:52 +0900

    Hard tab => Soft tab.

--- XmlDic.h
+++ XmlDic.h
@@ -13,41 +13,41 @@
 
 @interface XmlDic : NSObject <NSXMLParserDelegate>
 {
-	NSString*			_name;		//	名前 (name)
-	NSMutableString*	_value;		//	値 (value)
-	XmlDic*				_parent;	//	上位階層 (parent item)
-    NSMutableArray*		_items;		//	子要素 (child item)
+    NSString*           _name;      //  名前 (name)
+    NSMutableString*    _value;     //  値 (value)
+    XmlDic*             _parent;    //  上位階層 (parent item)
+    NSMutableArray*     _items;     //  子要素 (child item)
 }
 
 
-@property( nonatomic, retain ) NSString*		name;
-@property( nonatomic, retain ) NSMutableString*	value;
-@property( nonatomic, assign ) XmlDic*			parent;
-@property( nonatomic, retain ) NSMutableArray*	items;
+@property( nonatomic, retain ) NSString*          name;
+@property( nonatomic, retain ) NSMutableString*   value;
+@property( nonatomic, assign ) XmlDic*            parent;
+@property( nonatomic, retain ) NSMutableArray*    items;
 
 
-//	XML を取得してパースする
-//	Get a XML from the uri, and parse it.
+//  XML を取得してパースする
+//  Get a XML from the uri, and parse it.
 - (BOOL)parseWithURI:(NSString*)uri;
 
-//	XML を指定してパースする
-//	Parse the XML data.
+//  XML を指定してパースする
+//  Parse the XML data.
 - (void)parseWithData:(NSData*)data;
 
-//	名前の要素を全て取得する
-//	Get all items by the name.  
+//  名前の要素を全て取得する
+//  Get all items by the name.  
 - (NSMutableArray*)find:(NSString*)name;
 
-//	名前の要素を全て取得する
-//	Get all items by the name.  
+//  名前の要素を全て取得する
+//  Get all items by the name.  
 - (NSMutableArray*)find:(NSString*)name items:(NSMutableArray*)items;
 
-//	名前の要素を最初に見付けた1つ取得する
-//	Get first item only by the name.
+//  名前の要素を最初に見付けた1つ取得する
+//  Get first item only by the name.
 - (NSString*)first:(NSString*)name;
 
-//	名前と値が同じ要素を最初に見付けた1つ取得する
-//	Get first item only by the name, the tag and value.
+//  名前と値が同じ要素を最初に見付けた1つ取得する
+//  Get first item only by the name, the tag and value.
 - (XmlDic*)first:(NSString*)name tag:(NSString*)tag value:(NSString*)value;
 
 
